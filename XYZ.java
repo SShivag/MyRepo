c10dcac3c586eeab98079d806cfc86fc31e88109
SShivag <163106207+SShivag@users.noreply.github.com>
2024-10-07 10:41:41 +0530

    Print first string from list of stringd

--- XYZ.java
+++ XYZ.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class XYZ {
+    public static void main(String[] args) {
+
+
+        List<String> input = Arrays.asList("DTCC-test", "NSCC-test", "DTCC1-DTCC");
+
+        List<String> result = input.stream().map(s -> s.split("-")[0]).collect(Collectors.toList());
+        System.out.println(result);
+    }
 }
